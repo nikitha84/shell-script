a2978d9d22b6c2355fb23d46cb4d7bf1197c27f9
nikitha84 <nikitha84640@gmail.com>
2025-07-23 14:31:17 +0530

    mysqul,git

--- git.sh
+++ git.sh
@@ -4,9 +4,8 @@ ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"  #script name-date-.log
 
-R="\e[30m"
-G="\e[31m"
-Y="\e[32m"
+R="\e[31m"
+G="\e[32m"
 N="\e[0m"
 
 echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
